31ca6892793ea7017e8f2ca3fca4a0804ff4f7fc
Pedro Paulo Moreno Camargo <ppmcamargo@gmail.com>
2024-11-21 15:13:11 -0300

    ebnf

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
 # midi_lang
+
+# EBNF Grammar for MIDI Language
+
+## Program Structure
+```ebnf
+<PROGRAM> ::= <STATEMENT>* ;
+
+<STATEMENT> ::= <VARIABLE_DECLARATION> 
+              | <VARIABLE_ASSIGNMENT>
+              | <TEMPO_STATEMENT> 
+              | <NOTE_STATEMENT>
+              | <LOOP_STATEMENT>
+              | <IF_STATEMENT> ;
+
+<LOOP_STATEMENT> ::= "loop" "(" <RELATIONAL_EXPRESSION> ")" <BLOCK> ;
+
+<IF_STATEMENT> ::= "if" "(" <RELATIONAL_EXPRESSION> ")" <BLOCK> [ "else" <BLOCK> ] ;
+
+<BLOCK> ::= "{" <STATEMENT>* "}" ;
+
+<TEMPO_STATEMENT> ::= "tempo" "=" <EXPRESSION> ";" ;
+
+<NOTE_STATEMENT> ::= "play" <RELATIONAL_EXPRESSION> <RELATIONAL_EXPRESSION> [<RELATIONAL_EXPRESSION>] [ "at" <RELATIONAL_EXPRESSION>] ";" ;
+
+<VARIABLE_DECLARATION> ::= <TYPE> <IDENTIFIER> ["[" <RELATIONAL_EXPRESSION> "]"] [ "=" <RELATIONAL_EXPRESSION> ] ( "," <IDENTIFIER> [ "=" <RELATIONAL_EXPRESSION> ] )* ";" ;
+
+<VARIABLE_ASSIGNMENT> ::= <IDENTIFIER> ["[" <RELATIONAL_EXPRESSION> "]"] "=" <RELATIONAL_EXPRESSION> ";" ;
+
+<EXPRESSION> ::= <TERM> ( ("||" | "+" | "-") <TERM> )* ;
+
+<RELATIONAL_EXPRESSION> ::= <EXPRESSION> ( ("==" | "!=" | "<" | ">" | "<=" | ">=") <EXPRESSION> )? ;
+
+
+<TERM> ::= <FACTOR> ( ("*" | "/" | "&&") <FACTOR> )* ;
+
+
+<FACTOR> ::= ( ("+" | "-" | "!") <FACTOR> )
+           | <NUMBER>
+           | "(" <RELATIONAL_EXPRESSION> ")"
+           | <IDENTIFIER>
+           | <NOTE>
+           | <CHORD> ;
+
+
+
+<CHORD> ::= "[" <NOTE> ( "," <NOTE> )* "]" ;
+
+<NOTE> ::= <NOTE_NAME> <OCTAVE> ;
+
+<NOTE_NAME> ::= "A" | "B" | "C" | "D" | "E" | "F" | "G" [ "#" | "b" ] ;
+
+<OCTAVE> ::= "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" ;
+
+<DURATION> ::= <NUMBER> ;
+
+<IDENTIFIER> ::= <LETTER> (<LETTER> | <DIGIT> | "_")* ;
+
+<LETTER> ::= "a" | ... | "z" | "A" | ... | "Z" ;
+
+<NUMBER> ::= <DIGIT>+ ;
+
+<DIGIT> ::= "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" ;
+
+<TYPE> ::= "INT" | "NOTE" | "CHORD";
+```
--- ex.py
+++ ex.py
@@ -1,41 +1,55 @@
 from midiutil import MIDIFile
 
-# Cria um arquivo MIDI com uma única faixa
-midi = MIDIFile(1)
-
-# Configuração da faixa
+# Initialize MIDI file
+midi = MIDIFile(1)  # Single track
 track = 0
-time = 0  # Começa no tempo 0
+current_time = 0  # Start time in beats
 channel = 0
-tempo = 120  # BPM
-volume = 100  # Volume (0-127)
+default_velocity = 100
+current_tempo = 120  # Default tempo in BPM
 
-# Adiciona informações da faixa e do tempo
-midi.addTrackName(track, time, "Track 1")
-midi.addTempo(track, time, tempo)
+# Function to calculate note duration in seconds based on tempo
+def duration_in_seconds(duration_in_beats, tempo):
+    seconds_per_beat = 60 / tempo
+    return duration_in_beats * seconds_per_beat
 
-# Função para converter notas para números MIDI
+# Function to convert note names to MIDI pitch numbers
 def note_to_midi(note):
     note_mapping = {"C": 0, "D": 2, "E": 4, "F": 5, "G": 7, "A": 9, "B": 11}
-    octave = int(note[-1])  # Pega a oitava, ex: "4" de "C4"
-    base_note = note_mapping[note[0]]  # Nota base (ex: C -> 0)
-    if "#" in note:  # Se tiver sustenido
+    octave = int(note[-1])
+    base_note = note_mapping[note[0]]
+    if "#" in note:
         base_note += 1
-    elif "b" in note:  # Se tiver bemol
+    elif "b" in note:
         base_note -= 1
-    return (octave + 1) * 12 + base_note  # Calcula o número MIDI
+    return (octave + 1) * 12 + base_note
 
-# Define um acorde (exemplo: C maior = C4, E4, G4)
-chord = ["C4", "E4", "G4"]  # Notas do acorde
-duration = 2  # Duração do acorde em beats
+# Parse AST (Example)
+ast = [
+    {"command": "tempo", "value": 120},
+    {"command": "play", "note": "C4", "duration": 4, "velocity": 100},
+    {"command": "play", "note": "E4", "duration": 2, "velocity": 80},
+    {"command": "play", "note": "G4", "duration": 1, "velocity": 90}
+]
 
-# Adiciona todas as notas do acorde no mesmo instante
-for note in chord:
-    pitch = note_to_midi(note)
-    midi.addNote(track, channel, pitch, time, duration, volume)
+# Process AST
+for node in ast:
+    if node["command"] == "tempo":
+        current_tempo = node["value"]
+    elif node["command"] == "play":
+        # Get note information
+        pitch = note_to_midi(node["note"])
+        duration_in_beats = node["duration"]
+        velocity = node.get("velocity", default_velocity)
+        
+        # Add the note to the MIDI file
+        midi.addNote(track, channel, pitch, current_time, duration_in_beats, velocity)
+        
+        # Increment the current time based on the duration
+        current_time += duration_in_beats
 
-# Salva o arquivo MIDI
-with open("chord_example.mid", "wb") as output_file:
+# Save the MIDI file
+with open("output.mid", "wb") as output_file:
     midi.writeFile(output_file)
 
-print("Arquivo MIDI 'chord_example.mid' criado com sucesso!")
+print("MIDI file 'output.mid' created successfully!")
